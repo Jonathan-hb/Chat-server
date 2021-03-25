d59360a93f2aae9f0e0c0daf7fc733369e041c1d
Jonathan <jonathan@bangert.dk>
2021-03-25 19:04:48 +0100

    Added first models

--- chatBackend/models.py
+++ chatBackend/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class persons(models.Model);
+    class Meta:
+        verbose_name = "Person"
+        verbose_name_plural = "Persons"
+        #ordering = ["Jeg ved det ikke"]
+
+    full_name = models.CharField("Full name", max_length=20)
+    email = models.CharField("Email", max_length=100)
+    birthdate = models.DateField("Bithdate")
+    description = models.CharField("description", max_length=400, blank=True)
+    profile_picture = models.ImageField("Profiel Picture", blank=True)
+    nickname = models.CharField("Nickname", max_length=20)
+
+class messages(models.Model);
+    class Meta:
+        verbose_name = "Message"
+        verbose_name_plural = "Messages"
+        #ordering = ["Jeg ved det ikke"]
+
+    message = models.CharField("Message")
+    viewed = models.DateTimeField("Viewed", blank=True, None=True)
+    
+
