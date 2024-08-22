d52b465392651b1d4cf8e546a945a05eeb257456
Jonathan Bangert <jonathan@bangert.dk>
2024-08-21 20:12:50 -0600

    Update settings.py

--- chatServer/settings.py
+++ chatServer/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'v2li$#wtajw9@hq&!w$=i8$yd6k=yevedfj22qwz=$^m=e%7&m'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
